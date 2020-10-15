e2b8f10f5599049365bd4b5146f9f013f9a35e4d
Louise Crow <louise.crow@gmail.com>
2020-10-15 17:56:35 +0100

    Update for python 3.5 - add f-strings support

--- script/add_council_websites.py
+++ script/add_council_websites.py
@@ -1,3 +1,4 @@
+# -*- coding: future_fstrings -*-
 import urllib.request
 import csv
 from os.path import join
--- script/preprocess.py
+++ script/preprocess.py
@@ -1,3 +1,4 @@
+# -*- coding: future_fstrings -*-
 from os.path import join, basename, splitext, isfile
 import os
 import sys
