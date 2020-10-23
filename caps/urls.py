504a81992dd81f3d5ea862435496eac07f9430f7
Louise Crow <louise.crow@gmail.com>
2020-10-23 17:12:06 +0100

    Add some basic urls and views

--- caps/urls.py
+++ caps/urls.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 import caps.views as views
 
 urlpatterns = [
-    path('test', views.test_view, name='test-view'),
+    path('', views.HomePageView.as_view(), name='home'),
+    path('councils/<slug:slug>/', views.CouncilDetailView.as_view(), name='council'),
+    path('search/', views.SearchResultsView.as_view(), name='search_results'),
+    path('councils/', views.CouncilListView.as_view(), name='council_list'),
     path('admin/', admin.site.urls),
 ]
--- caps/views.py
+++ caps/views.py
@@ -1,9 +1,35 @@
+# -*- coding: future_fstrings -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import DetailView, ListView, TemplateView
+from django.db.models import Q, Count
 
+from caps.models import Council, PlanDocument
 
-# Create your views here.
+class HomePageView(TemplateView):
 
+    template_name = "home.html"
 
-def test_view(request):
-    return HttpResponse("test view")
+class CouncilDetailView(DetailView):
+
+    model = Council
+    template_name = 'council_detail.html'
+
+class CouncilListView(ListView):
+
+    model = Council
+    template_name = 'council_list.html'
+
+    def get_queryset(self):
+        return Council.objects.annotate(num_plans=Count('plandocument'))
+
+class SearchResultsView(ListView):
+
+    model = Council
+
+    template_name = 'search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list = Council.objects.filter(Q(plandocument__text__icontains=query))
+        return object_list
