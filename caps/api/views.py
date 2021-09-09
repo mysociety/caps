ac0563bb5a0b3ed38c9845db2a20e7f213c80948
Struan Donald <struan@exo.org.uk>
2021-09-09 14:29:15 +0100

    add filters to search term API end point

--- caps/api/views.py
+++ caps/api/views.py
@@ -1,10 +1,30 @@
 # -*- coding: future_fstrings -*-
+from datetime import datetime
+from django.utils.timezone import make_aware
 from django.db.models import Count
+from django.forms import ValidationError
 
 from rest_framework import viewsets, routers
+from rest_framework.exceptions import APIException
 
 from caps.models import Council, SavedSearch
 from caps.api.serializers import CouncilSerializer, SearchTermSerializer
+
+class InvalidParamException(APIException):
+    status_code = 400
+    default_status = "bad_request"
+
+    def __init__(self, field):
+        self.default_detail = self.default_detail % field
+        super().__init__()
+
+class InvalidCountException(InvalidParamException):
+    default_detail = "%s must be an integer greater than 0"
+
+
+class InvalidDateException(InvalidParamException):
+    default_detail = "%s must be a valid date with the format YYYY-MM-DD"
+
 
 class APIView(routers.APIRootView):
     """
@@ -33,8 +53,59 @@ class SearchTermViewSet(viewsets.ReadOnlyModelViewSet):
     * user_query - term searched for
     * result_count - number of results returned from search
     * times_seen - number of times term was searched for
+
+    Can optionally filter against the following query parameters in the URL:
+
+    * start_date - only include queries made on or after this date in YYYY-MM-DD format
+    * end_date - only include queries made on or before this date in YYYY-MM-DD format
+    * min_count - only include queries that have been made at least this number of times
+    * min_results - only include queries that have returned at least this many results
+
+    NB: all filter terms are inclusive so e.g. a start_date of 2021-08-12 will include
+    searches made on that date.
     """
 
     queryset = SavedSearch.objects.filter(result_count__gt=0).values('user_query', 'result_count').distinct().annotate(times_seen=Count('user_query'))
     serializer_class = SearchTermSerializer
+
+    def get_queryset(self):
+        queryset = SavedSearch.objects.filter(result_count__gt=0).values('user_query', 'result_count').distinct().annotate(times_seen=Count('user_query'))
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
+        min_count = self.request.query_params.get('min_count')
+        min_results = self.request.query_params.get('min_results')
+
+        if start_date is not None:
+            try:
+                start_date = datetime.strptime(start_date, "%Y-%m-%d")
+                queryset = queryset.filter(created__date__gte=make_aware(start_date))
+            except ValueError:
+                raise InvalidDateException(field='start_date')
+
+        if end_date is not None:
+            try:
+                end_date = datetime.strptime(end_date, "%Y-%m-%d")
+                queryset = queryset.filter(created__date__lte=make_aware(end_date))
+            except ValueError:
+                raise InvalidDateException(field='end_date')
+
+        if min_count is not None:
+            try:
+                min_count = int(min_count)
+                if min_count < 1:
+                    raise InvalidCountException(field='min_count')
+                queryset = queryset.filter(times_seen__gte=min_count)
+            except ValueError as error:
+                raise InvalidCountException(field='min_count')
+
+        if min_results is not None:
+            try:
+                min_results = int(min_results)
+                if min_results < 1:
+                    raise InvalidCountException(field='min_results')
+                queryset = queryset.filter(result_count__gte=min_results)
+            except ValueError as error:
+                raise InvalidCountException(field='min_results')
+
+        return queryset
 
--- caps/tests/test_api.py
+++ caps/tests/test_api.py
@@ -1,6 +1,8 @@
 import json
+from django.utils.timezone import make_aware
+from datetime import datetime
 from rest_framework.test import APITestCase
-from caps.models import Council, PlanDocument
+from caps.models import Council, PlanDocument, SavedSearch
 
 class CouncilsAPITestCase(APITestCase):
 
@@ -95,4 +97,182 @@ class CouncilsAPITestCase(APITestCase):
                 'authority_type': 'Unitary Authority',
                 'plan_count': 0,
             } ]
+        )
+
+
+class SearchTermAPITest(APITestCase):
+
+    def test_basic_searchterm_api(self):
+        SavedSearch.objects.create(
+            user_query="query",
+            result_count=2
+        )
+
+        response = self.client.get('/api/searchterms/')
+        self.assertEquals(json.loads(response.content),
+            [ {
+                'user_query': "query",
+                'result_count': 2,
+                'times_seen': 1
+            } ]
+        )
+
+    def test_term_aggregation(self):
+        SavedSearch.objects.create(
+            user_query="query",
+            result_count=2
+        )
+        SavedSearch.objects.create(
+            user_query="query",
+            result_count=2
+        )
+
+        response = self.client.get('/api/searchterms/')
+        self.assertEquals(json.loads(response.content),
+            [ {
+                'user_query': "query",
+                'result_count': 2,
+                'times_seen': 2
+            } ]
+        )
+
+    def test_result_count_filtering(self):
+        SavedSearch.objects.create(
+            user_query="query",
+            result_count=2
+        )
+        SavedSearch.objects.create(
+            user_query="query",
+            result_count=2
+        )
+        SavedSearch.objects.create(
+            user_query="another query",
+            result_count=1
+        )
+
+        response = self.client.get('/api/searchterms/?min_results=2')
+        self.assertEquals(json.loads(response.content),
+            [ {
+                'user_query': "query",
+                'result_count': 2,
+                'times_seen': 2
+            } ]
+        )
+
+    def test_seen_count_filtering(self):
+        SavedSearch.objects.create(
+            user_query="query",
+            result_count=2
+        )
+        SavedSearch.objects.create(
+            user_query="query",
+            result_count=2
+        )
+        SavedSearch.objects.create(
+            user_query="another query",
+            result_count=1
+        )
+
+        response = self.client.get('/api/searchterms/?min_count=2')
+        self.assertEquals(json.loads(response.content),
+            [ {
+                'user_query': "query",
+                'result_count': 2,
+                'times_seen': 2
+            } ]
+        )
+
+    def test_date_filtering(self):
+        created = make_aware(datetime(2021, 1, 4))
+        term = SavedSearch.objects.create(
+            user_query="query",
+            result_count=2
+        )
+        term.created = created
+        term.save()
+
+        created = make_aware(datetime(2021, 1, 1, 10, 10, 10))
+        term = SavedSearch.objects.create(
+            user_query="query",
+            result_count=2
+        )
+        term.created = created
+        term.save()
+
+        created = make_aware(datetime(2021, 1, 2))
+        term = SavedSearch.objects.create(
+            user_query="other",
+            result_count=3
+        )
+        term.created = created
+        term.save()
+
+        response = self.client.get('/api/searchterms/?start_date=2021-01-03')
+        self.assertEquals(json.loads(response.content),
+            [ {
+                'user_query': "query",
+                'result_count': 2,
+                'times_seen': 1
+            } ]
+        )
+
+        response = self.client.get('/api/searchterms/?end_date=2021-01-01')
+        self.assertEquals(json.loads(response.content),
+            [ {
+                'user_query': "query",
+                'result_count': 2,
+                'times_seen': 1
+            } ]
+        )
+
+        response = self.client.get('/api/searchterms/?start_date=2021-01-02&end_date=2021-01-03')
+        self.assertEquals(json.loads(response.content),
+            [ {
+                'user_query': "other",
+                'result_count': 3,
+                'times_seen': 1
+            } ]
+        )
+
+    def test_errors(self):
+        response = self.client.get('/api/searchterms/?start_date=21-01-02')
+        self.assertEquals(json.loads(response.content),
+            {
+                'detail': 'start_date must be a valid date with the format YYYY-MM-DD'
+            }
+        )
+
+        response = self.client.get('/api/searchterms/?start_date=21-01-32')
+        self.assertEquals(json.loads(response.content),
+            {
+                'detail': 'start_date must be a valid date with the format YYYY-MM-DD'
+            }
+        )
+
+        response = self.client.get('/api/searchterms/?min_count=0')
+        self.assertEquals(json.loads(response.content),
+            {
+                'detail': 'min_count must be an integer greater than 0'
+            }
+        )
+
+        response = self.client.get('/api/searchterms/?min_count=two')
+        self.assertEquals(json.loads(response.content),
+            {
+                'detail': 'min_count must be an integer greater than 0'
+            }
+        )
+
+        response = self.client.get('/api/searchterms/?min_results=0')
+        self.assertEquals(json.loads(response.content),
+            {
+                'detail': 'min_results must be an integer greater than 0'
+            }
+        )
+
+        response = self.client.get('/api/searchterms/?min_results=two')
+        self.assertEquals(json.loads(response.content),
+            {
+                'detail': 'min_results must be an integer greater than 0'
+            }
         )
