fab5b3e9656cb46feaaad74bae0faaa52970973c
Struan Donald <struan@exo.org.uk>
2021-11-23 16:35:01 +0000

    add an exact match option to the plan text search
    This uses the text_exact field in solr which has stemming turned off in
    so we get a genuine exact match

--- caps/forms.py
+++ caps/forms.py
@@ -1,8 +1,11 @@
-from django.forms import CharField
+from django.forms import CharField, BooleanField
 from haystack.forms import SearchForm
+from haystack.query import SearchQuerySet
+from haystack.inputs import Exact
 
 class HighlightedSearchForm(SearchForm):
     council_name=CharField(required=False)
+    exact=BooleanField(required=False)
 
     def search(self):
         kwargs = {
@@ -11,7 +14,17 @@ class HighlightedSearchForm(SearchForm):
             'hl.fragsize': 400,
             'hl.snippets': 3
         }
-        sqs = super(HighlightedSearchForm, self).search()
+        sqs = self.no_query_found()
+
+        # use the text_exact field in solr which doesn't stem
+        if self.cleaned_data['q'] and self.cleaned_data['exact'] is True:
+            query = self.cleaned_data['q']
+            kwargs['hl.fl'] = 'text_exact'
+            sqs = SearchQuerySet().filter(text_exact=Exact(query))
+            if self.load_all:
+                sqs = sqs.load_all()
+        else:
+            sqs = super(HighlightedSearchForm, self).search()
 
         if self.cleaned_data['council_name']:
             # narrow makes use of fq rather than q
