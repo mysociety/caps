dfbc9f1273508cb16c799d2fcbe6087460226a9b
Struan Donald <struan@exo.org.uk>
2021-12-08 15:44:26 +0000

    display all council league scores on the front page
    Very minimal styling etc, just gets them in place

--- scoring/views.py
+++ scoring/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.db.models import Subquery, OuterRef
 
-from django.views.generic import TemplateView
+from caps.models import Council
+from scoring.models import PlanScore, PlanSection, PlanSectionScore
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
     template_name = "scoring/home.html"
+
+    def get_queryset(self):
+        sort = self.request.GET.get('sort_by')
+        qs = Council.objects.annotate(
+            score=Subquery(
+                PlanScore.objects.filter(council_id=OuterRef('id'),year='2021').values('weighted_total')
+            )
+        )
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        councils = context['object_list'].values()
+        context['plan_sections'] = PlanSection.objects.filter(year=2021).all()
+
+        all_scores = PlanSectionScore.get_all_council_scores()
+
+        for council in councils:
+            council['all_scores'] = all_scores[council['id']]
+
+        codes = PlanSection.section_codes()
+
+        context['council_data'] = councils
+        return context
