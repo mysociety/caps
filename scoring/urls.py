97b1fe381fae8f8a4280eddb0f7d06226bddd837
Struan Donald <struan@exo.org.uk>
2021-12-08 15:47:57 +0000

    page showing all councils answers for a question
    Page to list all the responses from each council for a particular
    question

--- scoring/urls.py
+++ scoring/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.HomePageView.as_view(), name="home"),
     path('scoring/<str:council_type>/', views.HomePageView.as_view(), name='scoring'),
     path('councils/<slug:slug>/answers', views.CouncilAnswersView.as_view(), name='answers'),
+    path('answers/<slug:slug>/', views.AnswerCouncilsView.as_view(), name='answercouncils'),
 ]
