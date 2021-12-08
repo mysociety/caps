0da96c1550d21476b4d6418230eb3f3b29568b1d
Struan Donald <struan@exo.org.uk>
2021-12-08 15:47:09 +0000

    basic page to display all council answers/scores
    Page to list all the questions and the council's answers/scores for them

--- scoring/views.py
+++ scoring/views.py
@@ -1,8 +1,8 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.db.models import Subquery, OuterRef, Q
 
 from caps.models import Council
-from scoring.models import PlanScore, PlanSection, PlanSectionScore
+from scoring.models import PlanScore, PlanSection, PlanSectionScore, PlanQuestion
 
 from scoring.forms import ScoringSort
 
@@ -64,4 +64,56 @@ class HomePageView(ListView):
 
         context['form'] = form
         context['council_data'] = councils
+        return context
+
+class CouncilAnswersView(DetailView):
+    model = Council
+    context_object_name = 'council'
+    template_name = 'council_answers.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        council = context.get('council')
+
+        plan = PlanScore.objects.get(council=council, year=2021)
+
+        # do this in raw SQL as otherwise we need a third query and an extra loop below
+        questions = PlanQuestion.objects.raw(
+            "select q.id, q.code, q.text, q.question_type, q.max_score, s.code as section_code, a.answer, a.score \
+            from scoring_planquestion q join scoring_plansection s on q.section_id = s.id \
+            left join scoring_planquestionscore a on q.id = a.plan_question_id \
+            where s.year = '2021' and ( a.plan_score_id = %s or a.plan_score_id is null) order by q.code",
+            [council.id]
+        )
+
+        section_qs = PlanSectionScore.objects.select_related('plan_section').filter(
+            plan_score__council=context['council'],
+            plan_section__year=2021
+        )
+
+        sections = {}
+        for section in section_qs.all():
+            sections[section.plan_section.code] = {
+                'description': section.plan_section.description,
+                'max_score': section.plan_section.max_score,
+                'score': section.score,
+                'answers': []
+            }
+
+        for question in questions:
+            section = question.section_code
+            q = {
+                'code': question.code,
+                'display_code': question.code.replace('{}_'.format(question.section_code), ''),
+                'question': question.text,
+                'type': question.question_type,
+                'max': question.max_score,
+                'section': question.section.code,
+                'answer': question.answer or '-',
+                'score': question.score or 0,
+            }
+            sections[section]['answers'].append(q)
+
+        context['plan'] = plan
+        context['sections'] = sections
         return context
