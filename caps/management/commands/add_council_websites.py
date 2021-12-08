65badbc8f33d8ded12fa10bd2faecff88b251ee2
Alex Parsons <alex.parsons@mysociety.org>
2021-12-08 16:37:31 +0000

    Explicit wikidata user agent
    Technically this was already required, but calling from github's IP
    caused problems without it.

--- caps/management/commands/add_council_websites.py
+++ caps/management/commands/add_council_websites.py
@@ -78,7 +78,8 @@ def get_scotland():
     return councils
 
 def get_scotland_twitter():
-    headers = { 'Accept': 'text/csv' }
+    headers = {'Accept': 'text/csv',
+               'User-Agent': "CAPE Provisioning/0.0 (data.climateemergency.org)"}
     url = 'https://query.wikidata.org/sparql?query=SELECT%20%3Fitem%20%3FitemLabel%20%3Ftwitter%20%20WHERE%20%7B%0A%20%20%3Fitem%20wdt%3AP31%20wd%3AQ21451686%20.%0A%20%20%3Fitem%20wdt%3AP2002%20%3Ftwitter%20.%0A%20%20%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cen%22.%20%7D%0A%7D'
     r = requests.get(url, headers=headers)
     with open(SCOTTISH_TWITTER_CSV, 'wb') as outfile:
