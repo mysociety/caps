875451d2f190a7c77165e285c918f2ea1ef4e1f6
Struan Donald <struan@exo.org.uk>
2021-12-09 16:43:49 +0000

    section page showing per section and average council scores

--- league/urls.py
+++ league/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('league/<str:council_type>/', views.HomePageView.as_view(), name='league'),
 
     path('councils/<slug:slug>/answers', views.CouncilAnswersView.as_view(), name='answers'),
+    path('councils/<slug:slug>/section', views.CouncilSectionView.as_view(), name='section'),
     path('answers/<slug:slug>/', views.AnswerCouncilsView.as_view(), name='answercouncils'),
 ]
