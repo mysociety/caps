63fc8fe401ff01def789ffdfd9ed7a83f749942b
Struan Donald <struan@exo.org.uk>
2021-12-23 15:18:09 +0000

    test zero scores for sections and plans averages correctly
    sections included, plans not

--- scoring/tests/test_views.py
+++ scoring/tests/test_views.py
@@ -3,6 +3,7 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 from caps.models import Council
+from scoring.models import PlanSectionScore, PlanScore
 
 
 class TestAnswerView(TestCase):
@@ -65,6 +66,112 @@ class TestAnswerView(TestCase):
         council = Council.objects.get(authority_code="WBS")
         council.authority_type = "NMD"
         council.save()
+
+        url = reverse("answers", urlconf="scoring.urls", args=["borsetshire"])
+        response = self.client.get(url, HTTP_HOST="scoring.example.com")
+        sections = response.context["sections"]
+
+        self.assertEquals(
+            sections,
+            {
+                "s1_gov": {
+                    "answers": [
+                        {
+                            "code": "s1_gov_q1",
+                            "display_code": "q1",
+                            "question": "This is a sub header",
+                            "answer": "-",
+                            "max": 0,
+                            "section": "s1_gov",
+                            "score": 0,
+                            "type": "HEADER",
+                        },
+                        {
+                            "code": "s1_gov_q1_sp1",
+                            "display_code": "q1_sp1",
+                            "question": "The answer is True or False",
+                            "answer": "True",
+                            "max": 1,
+                            "section": "s1_gov",
+                            "score": 1,
+                            "type": "CHECKBOX",
+                        },
+                    ],
+                    "avg": 13.7,
+                    "max_score": 19,
+                    "description": "Governance, development and funding",
+                    "score": 15,
+                },
+                "s2_m_a": {
+                    "answers": [],
+                    "avg": 8.7,
+                    "max_score": 18,
+                    "description": "Mitigation and adaptation",
+                    "score": 10,
+                },
+            },
+        )
+
+    def test_zero_sections_scores_used_in_averages(self):
+        council = Council.objects.get(authority_code="WBS")
+        section = PlanSectionScore.objects.get(
+            plan_score__council_id=council.id, plan_section__code="s1_gov"
+        )
+        section.score = 0
+        section.weighted_score = 0
+        section.save()
+
+        url = reverse("answers", urlconf="scoring.urls", args=["borsetshire"])
+        response = self.client.get(url, HTTP_HOST="scoring.example.com")
+        sections = response.context["sections"]
+
+        self.assertEquals(
+            sections,
+            {
+                "s1_gov": {
+                    "answers": [
+                        {
+                            "code": "s1_gov_q1",
+                            "display_code": "q1",
+                            "question": "This is a sub header",
+                            "answer": "-",
+                            "max": 0,
+                            "section": "s1_gov",
+                            "score": 0,
+                            "type": "HEADER",
+                        },
+                        {
+                            "code": "s1_gov_q1_sp1",
+                            "display_code": "q1_sp1",
+                            "question": "The answer is True or False",
+                            "answer": "True",
+                            "max": 1,
+                            "section": "s1_gov",
+                            "score": 1,
+                            "type": "CHECKBOX",
+                        },
+                    ],
+                    "avg": 10.2,
+                    "max_score": 19,
+                    "description": "Governance, development and funding",
+                    "score": 15,
+                },
+                "s2_m_a": {
+                    "answers": [],
+                    "avg": 9.8,
+                    "max_score": 18,
+                    "description": "Mitigation and adaptation",
+                    "score": 10,
+                },
+            },
+        )
+
+    def test_zero_plan_score_not_used_in_averages(self):
+        council = Council.objects.get(authority_code="WBS")
+        plan = PlanScore.objects.get(council=council)
+        plan.total = 0
+        plan.weighted_score = 0
+        plan.save()
 
         url = reverse("answers", urlconf="scoring.urls", args=["borsetshire"])
         response = self.client.get(url, HTTP_HOST="scoring.example.com")
