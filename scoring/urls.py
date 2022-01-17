58a661ce9d345419e1ea94ca53c122dcf26f20d4
Lucas <hello@lucascm.dev>
2022-01-17 13:15:19 +0000

    Added static pages and added links to navbar
    Added About page
    Added methodology
    Added Contact page

--- scoring/urls.py
+++ scoring/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('councils/<slug:slug>/', views.CouncilView.as_view(), name='council'),
     path('questions/<slug:slug>/', views.QuestionView.as_view(), name='question'),
     path('location/', views.LocationResultsView.as_view(), name='location_results'),
+    path('methodology/', views.MethodologyView.as_view(), name='methodology'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
 ]
