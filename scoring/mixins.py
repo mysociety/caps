c567febb3195e22207abca77523903bf9a5b1b7f
Struan Donald <struan@exo.org.uk>
2022-01-19 12:27:30 +0000

    add scorecards mixin enforce login if site private
    Mixin that will show non logged in users a down page if they are not
    logged in if SCORECARDS_PRIVATE is True in settings

---
+++ scoring/mixins.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.mixins import AccessMixin
+from django.conf import settings
+from django.shortcuts import redirect
+
+
+class CheckForDownPageMixin(AccessMixin):
+    login_url = '/login/'
+    redirect_field_name = 'redirect_to'
+
+    def dispatch(self, request, *args, **kwargs):
+        if getattr(settings, 'SCORECARDS_PRIVATE', False) == False:
+            return super().dispatch(request, *args, **kwargs)
+
+        if not request.user.is_authenticated:
+            return redirect('downpage')
+
+        return super().dispatch(request, *args, **kwargs)
