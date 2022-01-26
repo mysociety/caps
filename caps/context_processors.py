4ffa6ec4fe7550606e5e05edbca29e833481e7c4
Struan Donald <struan@exo.org.uk>
2022-01-26 14:40:21 +0000

    use analytics on scorecards if configured

--- caps/context_processors.py
+++ caps/context_processors.py
@@ -3,6 +3,6 @@ from django.conf import settings
 
 def analytics(request):
     return {
-        "GOOGLE_ANALYTICS": settings.GOOGLE_ANALYTICS
-        "GOOGLE_ANALYTICS_SCORECARDS": settings.GOOGLE_ANALYTICS_SCORECARDS
+        "GOOGLE_ANALYTICS": settings.GOOGLE_ANALYTICS,
+        "GOOGLE_ANALYTICS_SCORECARDS": settings.GOOGLE_ANALYTICS_SCORECARDS,
     }
