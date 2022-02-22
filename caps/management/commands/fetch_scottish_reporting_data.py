9ecaa69760d79b4221c0da217df666f8872e7842
Struan Donald <struan@exo.org.uk>
2022-02-22 16:50:34 +0000

    store details of fetched scottish data in csv

--- caps/management/commands/fetch_scottish_reporting_data.py
+++ caps/management/commands/fetch_scottish_reporting_data.py
@@ -1,11 +1,14 @@
-from os.path import join
+from os.path import join, split
 from time import sleep
 import glob
+import re
 
 import requests
 from urllib.parse import urljoin
 import urllib.request
 import urllib3
+
+import pandas as pd
 
 from bs4 import BeautifulSoup
 
@@ -15,6 +18,7 @@ from django.utils.text import slugify
 
 import ssl
 
+from caps.import_utils import add_authority_codes
 
 BASE_URL = "https://sustainablescotlandnetwork.org/"
 
@@ -22,14 +26,25 @@ ssl._create_default_https_context = ssl._create_unverified_context
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-
-
 class Command(BaseCommand):
     help = "fetch scottish council climate data"
+
+    councils = []
+    current_council = None
 
     def handle(self, *args, **options):
         print("getting scottish council climate data")
         self.fetch_all_reports()
+        csv_file = self.save_data_to_csv()
+        add_authority_codes(csv_file)
+
+    def save_data_to_csv(self):
+        df = pd.DataFrame(self.councils)
+
+        outfile = join(settings.SCOTTISH_DIR, "data_list.csv")
+        df.to_csv(open(outfile, "w"), index=False, header=True)
+
+        return outfile
 
     def get_dom(self, url):
         local_filename, headers = urllib.request.urlretrieve(url)
@@ -39,13 +54,11 @@ class Command(BaseCommand):
 
         return BeautifulSoup(page, "html.parser")
 
-
     def fetch_all_reports(self):
         index_url = "https://sustainablescotlandnetwork.org/reports?_=1645526681566&filterrific%5Bwith_category_id%5D=6"
 
         while index_url is not None:
             index_url = self.paginate_list(index_url)
-
 
     def paginate_list(self, index_url):
         try:
@@ -63,7 +76,6 @@ class Command(BaseCommand):
 
         return None
 
-
     def process_councils_on_page(self, dom):
         councils = dom.find_all("div", class_="report__index-text")
         for council in councils:
@@ -71,22 +83,50 @@ class Command(BaseCommand):
             link = urljoin(BASE_URL, link)
             self.get_reports_from_page(link)
 
+    def parse_name_and_year(self, text):
+        parts = re.match(
+            r"(?P<council>.*) Climate Change (?:Report )?(?:pilot year )?(?P<start>\d+)\D(?P<end>\d+)",
+            text,
+        )
+        council = parts.group("council")
+        # there's a few places where the names on the web page don't match up with our data for
+        # assigning authority codes so fix these
+        council = council.replace("&", "and")
+        council = council.replace("Renfewshire", "Renfrewshire")
+        council = council.replace(
+            "Comhairlie nan Eilean Siar", "Comhairle nan Eilean Siar"
+        )
+        return (council, parts.group("start"), parts.group("end"))
 
     def get_reports_from_page(self, url):
         dom = self.get_dom(url)
         links = dom.find_all("a", class_="spreadsheet__link")
         for link in links:
-            url = link["href"]
             text = link.text
-            url = urljoin(BASE_URL, url)
-            filename = slugify(text)
-            local_path = join(settings.SCOTTISH_DIR, filename)
-            if glob.glob("{}.*".format(local_path)):
-                print("{} already fetched".format(filename))
-                continue
-            self.get_document(url, filename)
-            sleep(1)
+            slug = slugify(text)
+            basename = join(settings.SCOTTISH_DIR, slug)
 
+            council, start_year, end_year = self.parse_name_and_year(text)
+
+            url = link["href"]
+            url = urljoin(BASE_URL, url)
+
+            files = glob.glob("{}.*".format(basename))
+            if files:
+                print("{} already fetched".format(slug))
+                filepath, filename = split(files[0])
+            else:
+                filename = self.get_document(url, slug)
+                sleep(1)
+
+            self.councils.append(
+                {
+                    "council": council,
+                    "start_year": start_year,
+                    "end_year": end_year,
+                    "file": filename,
+                }
+            )
 
     def get_file_type(self, content_type):
         content_type = content_type.lower()
@@ -109,7 +149,6 @@ class Command(BaseCommand):
             print("Unknown content type: " + content_type)
 
         return file_type
-
 
     def get_document(self, url, filename):
         headers = {
