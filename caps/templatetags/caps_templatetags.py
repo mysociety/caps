d4e91a6d378fe74a1179ad8394b058a0a703e1f1
Struan Donald <struan@exo.org.uk>
2022-05-03 12:12:17 +0100

    add filter for capitalising document titles
    The built in title filter capitalises after apostrophes which is a won't
    fix issue (https://code.djangoproject.com/ticket/22323) so add our own
    using string.capwords() which is better suited for plan document titles.

--- caps/templatetags/caps_templatetags.py
+++ caps/templatetags/caps_templatetags.py
@@ -1,5 +1,7 @@
 import re
 
+from string import capwords
+
 from django import template
 from django.template.defaultfilters import stringfilter
 
@@ -16,3 +18,14 @@ def domain_human(value):
 @stringfilter
 def url_human(value):
     return re.sub(r"^(https?:[/][/])?(www[.])?(.*)", r"\3", value)
+
+
+@register.filter
+@stringfilter
+def document_title(value):
+    """
+    The standard title filter regards apostrophes as a word boundary which
+    means you end up with things like "Aberdeen'S", so use capwords which
+    is a much better match for our use case.
+    """
+    return capwords(value)
