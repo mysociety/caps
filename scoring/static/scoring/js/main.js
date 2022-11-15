729540b11be9b04865f32ba378b906158d4d9e9e
Lucas <hello@lucascm.dev>
2022-11-15 09:51:11 +0000

    [Scorecards] Added content navbar

--- scoring/static/scoring/js/main.js
+++ scoring/static/scoring/js/main.js
@@ -274,3 +274,26 @@ forEachElement('.js-collapse-children', function(wrapper){
         wrapper.appendChild(details);
     }
 });
+
+var secondaryNavbarButton = document.getElementById("mobile-content-navbar");
+var secondaryNavbarContent = document.getElementById("content-navbar");
+secondaryNavbarButton.addEventListener('click', function(){
+    if(!secondaryNavbarContent.classList.contains('show-height')) {
+        secondaryNavbarButton.setAttribute('aria-expanded', true);
+        secondaryNavbarButton.classList.toggle('close');
+        secondaryNavbarContent.classList.toggle('show-height');
+        secondaryNavbarContent.style.height = "auto";
+        let height = secondaryNavbarContent.clientHeight + "px";
+        secondaryNavbarContent.style.height = "0px";
+        setTimeout(function(){
+            secondaryNavbarContent.style.height = height;
+        }, 0);
+    } else {
+        secondaryNavbarButton.setAttribute('aria-expanded', false);
+        secondaryNavbarButton.classList.remove('close');
+        secondaryNavbarContent.style.height = "0px";
+        secondaryNavbarContent.addEventListener('transitionend', function(){
+            secondaryNavbarContent.classList.remove('show-height');
+        }, {once: true})
+    }
+});
