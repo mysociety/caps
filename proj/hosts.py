8f9c292055319597ec6d6059c890d407167ad2f9
Struan Donald <struan@exo.org.uk>
2023-02-07 13:12:28 +0000

    [CAPE] allow cape.mysociety.org as a host name

--- proj/hosts.py
+++ proj/hosts.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-from django_hosts import patterns, host
+from django_hosts import host, patterns
 
 host_patterns = patterns(
     "",
-    host(r"data.climateemergency.uk", settings.ROOT_URLCONF, name="cape"),
+    host(r"cape.mysociety.org", settings.ROOT_URLCONF, name="cape"),
     host(r"((?:www.)?councilclimatescorecards)", "scoring.urls", name="scoring"),
 )
