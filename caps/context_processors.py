80b6f94cf3c52eb31e402dea42e849a276f3e458
Zarino Zappia <mail@zarino.co.uk>
2023-02-16 13:58:04 +0000

    [CAPE] Allow use of Google Tag Manager
    Setting a `GOOGLE_TAG_MANAGER` environment variable (instead of
    `GOOGLE_ANALYTICS`) will render the required GTM HTML snippets
    (instead of a GA4/gtag snippet) in the CAPE head/body.
    
    If you want to enable GTM in your Docker environment, you will want to
    define a GTM container ID in your `.env` file, like so:
    
         GOOGLE_TAG_MANAGER='GTM-#######'
    
    If you have a `GOOGLE_TAG_MANAGER` variable defined, you do not need a
    `GOOGLE_ANALYTICS` variable (since we assume the whole point of you
    using GTM is that you’d getting GTM to inject the Analyitcs gtag).
    
    Since all of these tracking variables should be optional, I’ve removed
    the default `xxx` values from the various config files. If none of the
    (optional) environment variables are present, then no tracking markup
    is rendered (rather than the previous behaviour, which was to render
    but with a non-functional `xxx` tracking code).

--- caps/context_processors.py
+++ caps/context_processors.py
@@ -4,5 +4,6 @@ from django.conf import settings
 def analytics(request):
     return {
         "GOOGLE_ANALYTICS": settings.GOOGLE_ANALYTICS,
+        "GOOGLE_TAG_MANAGER": settings.GOOGLE_TAG_MANAGER,
         "GOOGLE_ANALYTICS_SCORECARDS": settings.GOOGLE_ANALYTICS_SCORECARDS,
     }
