bdd9993b88b6f18b579ef213038b07d075068578
Alex Parsons <alex.parsons@mysociety.org>
2023-04-21 12:41:57 +0000

    Enable debug mode in google analytics in dev
    - Still needs to be given the measurement id
    - Means events will show up in DebugView

--- caps/context_processors.py
+++ caps/context_processors.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 def analytics(request):
     return {
+        "DEBUG": settings.DEBUG,
         "GOOGLE_ANALYTICS": settings.GOOGLE_ANALYTICS,
         "GOOGLE_TAG_MANAGER": settings.GOOGLE_TAG_MANAGER,
         "GOOGLE_ANALYTICS_SCORECARDS": settings.GOOGLE_ANALYTICS_SCORECARDS,
