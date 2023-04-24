4ad27755f77f7990a7bd1e442f07a400a4dda056
Alex Parsons <alex.parsons@mysociety.org>
2023-04-24 11:40:06 +0000

    Pass measurement protocol id into template

--- caps/context_processors.py
+++ caps/context_processors.py
@@ -5,6 +5,7 @@ def analytics(request):
     return {
         "DEBUG": settings.DEBUG,
         "GOOGLE_ANALYTICS": settings.GOOGLE_ANALYTICS,
+        "GOOGLE_MEASUREMENT_PROTOCOL_SECRET": settings.GOOGLE_MEASUREMENT_PROTOCOL_SECRET,
         "GOOGLE_TAG_MANAGER": settings.GOOGLE_TAG_MANAGER,
         "GOOGLE_ANALYTICS_SCORECARDS": settings.GOOGLE_ANALYTICS_SCORECARDS,
     }
