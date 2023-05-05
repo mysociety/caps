2086487d594ebef1a8e691bf1ea993d3099b1749
Alex Parsons <alex.parsons@mysociety.org>
2023-05-05 15:29:50 +0100

    Add measurement protocol for custom events (#506)
    Send custom events via measurement protocol.
    Needed because of gtag issues when running without cookies.
    
    ---------
    
    Co-authored-by: Zarino Zappia <mail@zarino.co.uk>

--- caps/context_processors.py
+++ caps/context_processors.py
@@ -3,7 +3,9 @@ from django.conf import settings
 
 def analytics(request):
     return {
+        "DEBUG": settings.DEBUG,
         "GOOGLE_ANALYTICS": settings.GOOGLE_ANALYTICS,
+        "GOOGLE_MEASUREMENT_PROTOCOL_SECRET": settings.GOOGLE_MEASUREMENT_PROTOCOL_SECRET,
         "GOOGLE_TAG_MANAGER": settings.GOOGLE_TAG_MANAGER,
         "GOOGLE_ANALYTICS_SCORECARDS": settings.GOOGLE_ANALYTICS_SCORECARDS,
     }
--- caps/static/caps/js/main.js
+++ caps/static/caps/js/main.js
@@ -224,26 +224,67 @@ $('form[data-ajax-feedback-submit]').on('submit', function(e){
     });
 });
 
-var trackEvent = function(eventName, params) {
-    // We'll return a promise, and resolve it when either Gtag handles
-    // our event, or a maximum fallback period elapses. Promises can
+var trackEvent = function(eventName, eventParams) {
+    // Custom events don't work in GA4 if you have { analytics_storage: "denied" }
+    // (ie: if you have cookies disabled). So we use the Measurement Protocol API
+    // to track custom events instead.
+    //
+    // trackEvent() returns a promise, and resolves it when either the MP API
+    // handles our event, or a maximum fallback period elapses. Promises can
     // only be resolved once, so this also ensures whatever callbacks
     // are attached to the promise only execute once.
+    //
+    // eventName should be a string. eventParams is an optional array of event
+    // parameters to be sent to Google Analytics.
+
     var dfd = $.Deferred();
 
     var callback = function(){
         dfd.resolve();
     };
 
-    // Tell Gtag to resolve our promise when it's done.
-    var params = $.extend(params, {
-        event_callback: callback
-    });
+    // Returns either an array, or undefined.
+    var measurementProtocolDetails = window.dataLayer.filter(function(row){
+        return row[0] === 'measurement_protocol_secret';
+    })[0];
 
-    gtag('event', eventName, params);
+    if (measurementProtocolDetails) {
+        var measurementParams = {
+            measurement_id: measurementProtocolDetails[1], // G-XXXXXXXXXX
+            api_secret: measurementProtocolDetails[2]
+        }
+        if (measurementProtocolDetails[3]) {
+            var eventParams = $.extend(params, {
+                debug_mode: '1'
+            });
+        }
 
-    // Wait a maximum of 2 seconds for Gtag to resolve promise.
-    setTimeout(callback, 2000);
+        // Set a random client_id (2 32-bit integers seperated by a dot).
+        // Note this random approach means the GA debugView won't work.
+        // (To get that to work, you need to turn back on the cookies,
+        // then use the same client_id as in in the _ga cookie.)
+        var client_id = Math.floor(Math.random() * 1000000000) + '.' + Math.floor(Math.random() * 1000000000);
+
+        // https://developers.google.com/analytics/devguides/collection/protocol/ga4/sending-events?client_type=gtag#send_an_event
+        $.ajax({
+            url: 'https://www.google-analytics.com/mp/collect?' + $.param(measurementParams),
+            method: 'POST',
+            data: JSON.stringify({
+                client_id: client_id,
+                events: [{
+                    name: eventName,
+                    params: eventParams
+                }]
+            })
+        }).always(callback);
+
+        // Wait a maximum of 2 seconds for ajax to resolve.
+        setTimeout(callback, 2000);
+
+    } else {
+        // Measurement Protocol not available. Resolve promise immediately.
+        callback();
+    }
 
     return dfd.promise();
 };
