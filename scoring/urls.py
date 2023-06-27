5db020ab3997c4aa099cc51e1e70c1c4e7983ec5
Struan Donald <struan@exo.org.uk>
2023-06-27 16:12:26 +0100

    fix methodology links and add scoring namespace

--- scoring/urls.py
+++ scoring/urls.py
@@ -4,32 +4,41 @@ from django.urls import include, path
 
 import scoring.views as views
 
+app_name = "scoring"
+scoring_patterns = (
+    [
+        path("", views.HomePageView.as_view(), name="home"),
+        path(
+            "scoring/<str:council_type>/", views.HomePageView.as_view(), name="scoring"
+        ),
+        path("councils/<slug:slug>/", views.CouncilView.as_view(), name="council"),
+        path("section/<slug:slug>/", views.SectionView.as_view(), name="section"),
+        path("location/", views.LocationResultsView.as_view(), name="location_results"),
+        path(
+            "plan-scorecards-2022/methodology/",
+            views.MethodologyView.as_view(),
+            name="methodology2022",
+        ),
+        path(
+            "methodology/",
+            views.Methodology2023View.as_view(),
+            name="methodology",
+        ),
+        path("about/", views.AboutView.as_view(), name="about"),
+        path("contact/", views.ContactView.as_view(), name="contact"),
+        path(
+            "how-to-use-the-scorecards/",
+            views.HowToUseView.as_view(),
+            name="how-to-use-the-scorecards",
+        ),
+        path("down/", views.DownPageView.as_view(), name="downpage"),
+        path("login/", views.LoginView.as_view(), name="login"),
+        path("logout/", views.LogoutView.as_view(), name="logout"),
+    ],
+    "scoring",
+)
 urlpatterns = [
-    path("", views.HomePageView.as_view(), name="home"),
-    path("scoring/<str:council_type>/", views.HomePageView.as_view(), name="scoring"),
-    path("councils/<slug:slug>/", views.CouncilView.as_view(), name="council"),
-    path("section/<slug:slug>/", views.SectionView.as_view(), name="section"),
-    path("location/", views.LocationResultsView.as_view(), name="location_results"),
-    path(
-        "plan-scorecards-2022/methodology/",
-        views.MethodologyView.as_view(),
-        name="methodology2022",
-    ),
-    path(
-        "methodology/",
-        views.Methodology2023View.as_view(),
-        name="methodology",
-    ),
-    path("about/", views.AboutView.as_view(), name="about"),
-    path("contact/", views.ContactView.as_view(), name="contact"),
-    path(
-        "how-to-use-the-scorecards/",
-        views.HowToUseView.as_view(),
-        name="how-to-use-the-scorecards",
-    ),
-    path("down/", views.DownPageView.as_view(), name="downpage"),
-    path("login/", views.LoginView.as_view(), name="login"),
-    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("", include(scoring_patterns)),
     path(
         "plan-scorecards-2022/",
         include(("scoring2022.urls", "scoring2022"), namespace="scoring2022"),
