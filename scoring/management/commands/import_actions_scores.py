738ebb175a35e098651a22b907f9e8a2b8f957a4
Struan Donald <struan@exo.org.uk>
2023-09-21 14:47:42 +0100

    import question and answer details

--- scoring/management/commands/import_actions_scores.py
+++ scoring/management/commands/import_actions_scores.py
@@ -35,6 +35,8 @@ class Command(BaseCommand):
     SCORECARD_DATA_DIR = Path(settings.DATA_DIR, "scorecard_data", str(YEAR))
     SECTION_SCORES_CSV = Path(SCORECARD_DATA_DIR, "raw_section_marks.csv")
     OVERALL_SCORES_CSV = Path(SCORECARD_DATA_DIR, "all_section_scores.csv")
+    QUESTIONS_CSV = Path(SCORECARD_DATA_DIR, "question_data.csv")
+    ANSWERS_CSV = Path(SCORECARD_DATA_DIR, "all_answer_data.csv")
 
     DEFAULT_TOP_PERFORMER_COUNT = 10
     TOP_PERFORMER_COUNT = {
@@ -177,8 +179,87 @@ class Command(BaseCommand):
                 section_score.top_performer = section.code
                 section_score.save()
 
+    def import_questions(self):
+        df = pd.read_csv(self.QUESTIONS_CSV)
+
+        section_codes = {name: code for code, name in self.SECTIONS.items()}
+
+        for index, row in df.iterrows():
+            code = section_codes[row["section"]] + "_q" + row["question_number"]
+
+            section = re.sub(r"(.*)_q[0-9].*", r"\1", code)
+            plan_section = None
+            try:
+                plan_section = PlanSection.objects.get(code=section, year=self.YEAR)
+            except PlanSection.DoesNotExist:
+                print("no section found for q {}, section {}".format(code, section))
+                continue
+
+            question, created = PlanQuestion.objects.get_or_create(
+                code=code, section=plan_section
+            )
+
+            question.max_score = row["max_score"]
+            question.text = row["description"]
+            question.question_type = row["type"]
+            question.weighting = row["weighting"]
+            question.how_marked = row["how_marked"]
+            question.save()
+
+    def import_question_scores(self):
+        # import related fields in bulk at the start
+        councils = {x.gss_code: x for x in Council.objects.all()}
+        plan_scores = {x.council: x for x in PlanScore.objects.filter(year=self.YEAR)}
+        questions = {x.code: x for x in PlanQuestion.objects.all()}
+
+        section_codes = {name: code for code, name in self.SECTIONS.items()}
+
+        df = pd.read_csv(self.ANSWERS_CSV)
+
+        # can fix at series level rather than testing individual entries
+        df["score"] = df["score"].fillna(0)
+        to_create = []
+
+        # more efficent just to delete everything and quickly reload
+        PlanQuestionScore.objects.filter(plan_score__year=self.YEAR).delete()
+
+        for index, row in df.iterrows():
+            code = section_codes[row["section"]] + "_q" + row["question-number"]
+
+            council = councils.get(row["local-authority-gss-code"], None)
+            plan_score = plan_scores.get(council, None)
+            question = questions.get(code, None)
+
+            if council is None:
+                print(
+                    "failed to match council {}".format(row["local-authority-gss-code"])
+                )
+                continue
+            if plan_score is None:
+                print("failed to match plan score for {}".format(council.name))
+                continue
+            if question is None:
+                print("failed to match question {}".format(code))
+                continue
+
+            links = ""
+            if not pd.isna(row["evidence"]):
+                links = row["evidence"]
+
+            score_obj = PlanQuestionScore(
+                plan_score=plan_score,
+                plan_question=question,
+                score=row["score"],
+                answer=row["answer"],
+                evidence_links=links,
+            )
+            to_create.append(score_obj)
+        PlanQuestionScore.objects.bulk_create(to_create)
+
     def handle(self, *args, **options):
         self.create_sections()
         self.import_section_scores()
         self.import_overall_scores()
         self.label_top_performers()
+        self.import_questions()
+        self.import_question_scores()
