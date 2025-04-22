fd1857d1e991ec3f4356c419894e6e3022c062d2
Lucas <hello@lucascm.dev>
2025-04-22 08:16:22 +0100

    [Scorecards] Added Nation page and Nation pages
    Adds pages to display scorecards data broken down by nations

--- scoring/views.py
+++ scoring/views.py
@@ -21,7 +21,7 @@ from caps.models import Council, PlanDocument, Promise
 from caps.utils import gen_natsort_lamda
 from caps.views import BaseLocationResultsView
 from conf.social_graphics import social_graphics
-from scoring.filters import PlanScoreFilter, QuestionScoreFilter
+from scoring.filters import NationPlanScoreFilter, PlanScoreFilter, QuestionScoreFilter
 from scoring.forms import ScoringSort, ScoringSortCA
 from scoring.mixins import (
     AdvancedFilterMixin,
@@ -68,13 +68,106 @@ class PrivacyView(TemplateView):
         return context
 
 
-@method_decorator(cache_control(**cache_settings), name="dispatch")
-class HomePageView(
+class BaseCouncilListView(
     PrivateScorecardsAccessMixin,
     SearchAutocompleteMixin,
     AdvancedFilterMixin,
     FilterView,
 ):
+    def get_missing_councils(self, council_ids, scoring_group):
+        return []
+
+    def get_averages(self, context):
+        scoring_group = self.get_scoring_group()
+        country = self.kwargs.get("nation_name")
+        if country:
+            country = Council.country_code(country)
+
+        averages = PlanSection.get_average_scores(
+            scoring_group=scoring_group,
+            filter=context.get("filter_params", None),
+            year=self.request.year.year,
+            country=country,
+        )
+
+        if self.request.year.previous_year:
+            previous_averages = PlanSection.get_average_scores(
+                scoring_group=scoring_group,
+                filter=context.get("filter_params", None),
+                year=self.request.year.previous_year.year,
+            )
+            for section in averages.keys():
+                if section != "total":
+                    averages[section]["change"] = (
+                        averages[section]["weighted"]
+                        - previous_averages[section]["weighted"]
+                    )
+
+        section_averages = []
+        for code in sorted(averages.keys()):
+            if code != "total":
+                section_averages.append(averages[code])
+
+        return averages, section_averages
+
+    def get_council_data(self, context):
+        scoring_group = self.get_scoring_group()
+        councils = context["object_list"].values()
+
+        all_scores = PlanSectionScore.get_all_council_scores(
+            plan_year=self.request.year.year, as_list=True
+        )
+
+        councils = list(councils.all())
+        council_ids = []
+        out = True
+        for council in councils:
+            council_ids.append(council["council_id"])
+            if out:
+                out = False
+            council["all_scores"] = all_scores[council["council_id"]]
+            if council["score"] is not None:
+                council["percentage"] = council["score"]
+            else:
+                council["percentage"] = 0
+
+        if context.get("filter_params", None) is None:
+            missing_councils = self.get_missing_councils(council_ids, scoring_group)
+
+            for council in missing_councils:
+                councils.append(
+                    {
+                        "score": None,
+                        "name": council.name,
+                        "slug": council.slug,
+                        "authority_code": council.authority_code,
+                        "top_performer": None,
+                    }
+                )
+        return councils
+
+    def get_sorted_councils(self, form, councils):
+        sorted_by = None
+        if form.is_valid():
+            sort = form.cleaned_data["sort_by"]
+            if sort != "":
+                sorted_by = sort
+                councils = sorted(
+                    councils,
+                    key=lambda council: (
+                        0
+                        if council["score"] == 0 or council["score"] is None
+                        else council["all_scores"][sort]["score"]
+                    ),
+                    reverse=True,
+                )
+                councils = sorted(councils, key=itemgetter("slug"))
+
+        return sorted_by, councils
+
+
+@method_decorator(cache_control(**cache_settings), name="dispatch")
+class HomePageView(BaseCouncilListView):
     filterset_class = PlanScoreFilter
     template_name = "scoring/home.html"
 
@@ -154,11 +247,8 @@ class HomePageView(
 
         context = self.setup_filter_context(context, context["filter"], scoring_group)
 
-        averages = PlanSection.get_average_scores(
-            scoring_group=scoring_group,
-            filter=context.get("filter_params", None),
-            year=self.request.year.year,
-        )
+        averages, section_averages = self.get_averages(context)
+
         all_scores = PlanSectionScore.get_all_council_scores(
             plan_year=self.request.year.year, as_list=True
         )
@@ -1607,3 +1697,261 @@ class NotFoundPageView(TemplateView):
             status=404,
             **response_kwargs,
         )
+
+
+class NationListView(TemplateView):
+    template_name = "scoring/nation_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        council_types = (
+            Council.SCORING_GROUPS["single"]["types"]
+            + Council.SCORING_GROUPS["northern-ireland"]["types"]
+        )
+        scores = (
+            PlanScore.objects.filter(
+                year=self.request.year.year,
+                council__authority_type__in=council_types,
+            )
+            .values("council__country")
+            .annotate(avg=Avg("weighted_total"))
+            .values("council__country", "avg")
+        )
+
+        country_averages = {}
+        for score in scores:
+            country_averages[Council.country_description(score["council__country"])] = (
+                round(score["avg"], None)
+            )
+
+        # Theoretically we could get the country names (but not slugs)
+        # out of models.Council.COUNTRY_CHOICES, but it doesn’t seem worth it,
+        # when we need to add in so much extra stuff anyway.
+        context["nations"] = [
+            {
+                "slug": "england",
+                "name": "England",
+                "statistic_value": f"{country_averages['England']}%",
+                "statistic_label": "Average Single Tier council score",
+                "description": "We've summarised the key findings from across the 324 English councils and mayoral authorities to provide an overview of the results from the 2025 Council Climate Action Scorecards.",
+                "cta_label": "View English scores",
+            },
+            {
+                "slug": "scotland",
+                "name": "Scotland",
+                "statistic_value": f"{country_averages['Scotland']}%",
+                "statistic_label": "Average score",
+                "description": "We've summarised the key findings from across the councils in Scotland to provide a Scottish overview of these councils' results in the 2025 Council Climate Action Scorecards.",
+                "cta_label": "View Scottish scores",
+            },
+            {
+                "slug": "northern-ireland",
+                "name": "Northern Ireland",
+                "statistic_value": f"{country_averages['Northern Ireland']}%",
+                "statistic_label": "Average score",
+                "description": "We've summarised the key findings from the 2025 Council Climate Action Scorecards to provide a comprehensive overview of Northern Irish councils' climate action progress.",
+                "cta_label": "View Northern Irish scores",
+            },
+            {
+                "slug": "wales",
+                "name": "Wales",
+                "statistic_value": f"{country_averages['Wales']}%",
+                "statistic_label": "Average score",
+                "description": "We've summarised the key findings from across the 22 councils in Wales to provide a Welsh overview of these councils' results in the 2025 Council Climate Action Scorecards.",
+                "cta_label": "View Welsh scores",
+            },
+        ]
+
+        context["page_title"] = "Nations"
+        context["plan_year"] = self.request.year
+
+        return context
+
+
+class NationDetailView(BaseCouncilListView):
+    filterset_class = NationPlanScoreFilter
+    template_name = "scoring/nation_detail.html"
+
+    def get_scoring_group(self):
+        country = self.kwargs["nation_name"]
+        scoring_group_slug = self.kwargs.get("council_type", "")
+        group = None
+
+        if country == "england":
+            try:
+                group = Council.SCORING_GROUPS[scoring_group_slug]
+            except:
+                group = Council.SCORING_GROUPS["single"]
+        elif country == "northern-ireland":
+            group = Council.SCORING_GROUPS["northern-ireland"]["types"]
+        else:
+            group = Council.SCORING_GROUPS["single"]
+
+        return group
+
+    def get_queryset(self):
+        nation = self.kwargs["nation_name"]
+        scoring_group = self.get_scoring_group()
+
+        filters = {
+            "year": self.request.year.year,
+            "council__country": Council.country_code(nation),
+            "council__authority_type__in": scoring_group["types"],
+        }
+
+        qs = (
+            PlanScore.objects.filter(**filters)
+            .annotate(
+                score=F("weighted_total"),
+                name=F("council__name"),
+                slug=F("council__slug"),
+                authority_code=F("council__authority_code"),
+            )
+            .order_by(F("weighted_total").desc(nulls_last=True))
+        )
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        scoring_group = self.get_scoring_group()
+        councils = self.get_council_data(context)
+        averages, section_averages = self.get_averages(context)
+        context["averages"] = averages
+        context["section_averages"] = section_averages
+        context = self.setup_filter_context(context, context["filter"], scoring_group)
+
+        form_class = ScoringSort
+        form = form_class(self.request.GET)
+        sorted_by, councils = self.get_sorted_councils(form, councils)
+
+        context["council_data"] = councils
+        context["form"] = form
+        context["sorted_by"] = sorted_by
+        nation = self.kwargs["nation_name"]
+        context["section_link_template"] = "scoring/includes/section_link_current.html"
+
+        nations = {
+            "england": {
+                "slug": "england",
+                "name": "England",
+                "description": """
+                    We’ve summarised the key findings from across the 324 English councils and mayoral authorities to provide an overview of the results from the 2025 Council Climate Action Scorecards.
+
+                    Whilst the scores vary across England, it is noticeable that, of the lowest scoring councils in the UK, 80% are in England. There are low scoring councils across all four types of English authorities: district, county, single tier and mayoral, with at least 10% of councils across each type of English authority scoring 20% or below. And, whilst most legislation is the same across all four nations, in England there is no statutory duty for councils to act on climate.
+                """,
+            },
+            "scotland": {
+                "slug": "scotland",
+                "name": "Scotland",
+                "description": """
+                    We've summarised the key findings from across the councils in Scotland to provide a Scottish overview of these councils' results in the 2025 Council Climate Action Scorecards.
+
+                    Whilst the scores vary across Scotland, it is noticeable that, of the lowest scoring councils in the UK, none are Welsh or Scottish. Whilst most legislation is the same across all 4 nations, in Scotland they have set a net zero target for 2045 and Scottish councils have a statutory duty to contribute towards this, as well as to report on their emissions.
+                """,
+            },
+            "northern-ireland": {
+                "slug": "northern-ireland",
+                "name": "Northern Ireland",
+                "description": """
+                    We’ve summarised the key findings from the 2025 Council Climate Action Scorecards  to provide an overview of Northern Irish councils’ climate action.
+
+                    The scores vary across Northern Ireland, and on average, Northern Irish councils score lower than other nations in the UK. Northern Irish councils have fewer powers than other councils in the UK in relation to schools, housing and transport. Whilst most legislation is the same across all four nations, Northern Irish and English councils (unlike in Wales and Scotland) do not have a statutory duty to work towards net zero across their whole operations or council area.
+                """,
+            },
+            "wales": {
+                "slug": "wales",
+                "name": "Wales",
+                "description": """
+                    We’ve summarised the key findings from across the 22 councils in Wales to provide a Welsh overview of these councils’ results in the 2025 Council Climate Action Scorecards.
+
+                    Whilst the scores vary across Wales, it is noticeable that, of the lowest scoring councils in the UK, none are Welsh or Scottish. And, whilst most legislation is the same across all 4 nations, in Wales in 2017, the Welsh Government set the ambition of achieving a carbon neutral public sector by 2030, which includes Welsh councils.
+                """,
+            },
+        }
+
+        social_graphics = {
+            "england": {
+                "pdf": {
+                    "src_pdf": "scoring/img/social-graphics/england/scorecards-england.pdf",
+                    "src_jpg": "scoring/img/social-graphics/england/england-graphic.jpg",
+                    "height": 1159,
+                    "width": 2100,
+                },
+                "zip": "scoring/img/social-graphics/england/england-graphics.zip",
+                "images": [
+                    {
+                        "src_facebook": "scoring/img/social-graphics/england/facebook-1@2x.png",
+                        "src_instagram": "scoring/img/social-graphics/england/instagram-1@2x.png",
+                        "alt": "England Leading the Way; 79% of councils have reduced mowing or created wildflower habitat in their area; 85% of councils, including all county councils, have a named climate Portfolio Holder",
+                    },
+                    {
+                        "src_facebook": "scoring/img/social-graphics/england/facebook-2@2x.png",
+                        "src_instagram": "scoring/img/social-graphics/england/instagram-2@2x.png",
+                        "alt": "England Leading the Way; 83% of councils help residents retrofit their homes; 81% of councils have a Climate Action Plan with SMART targets",
+                    },
+                    {
+                        "src_facebook": "scoring/img/social-graphics/england/facebook-3@2x.png",
+                        "src_instagram": "scoring/img/social-graphics/england/instagram-3@2x.png",
+                        "alt": "England Room for Improvement; 60% of English planning authorities have set the highest water efficiency standards for new builds",
+                    },
+                    {
+                        "src_facebook": "scoring/img/social-graphics/england/facebook-4@2x.png",
+                        "src_instagram": "scoring/img/social-graphics/england/instagram-4@2x.png",
+                        "alt": "England Room for Improvement; 62% of transport authorities have low-emission buses used on routes; 62% of councils produced an annual Climate Action Plan update report",
+                    },
+                    {
+                        "src_facebook": "scoring/img/social-graphics/england/facebook-5@2x.png",
+                        "src_instagram": "scoring/img/social-graphics/england/instagram-5@2x.png",
+                        "alt": "England Falling Behind; 17% of English planning authorities have set net zero standards for building new housing; 7% of councils have adopted a detailed decision making process that puts the climate crisis at the heart of all council decisions",
+                    },
+                    {
+                        "src_facebook": "scoring/img/social-graphics/england/facebook-6@2x.png",
+                        "src_instagram": "scoring/img/social-graphics/england/instagram-6@2x.png",
+                        "alt": "England Falling Behind; Only 7 English councils recycle more than 60% of their waste; No councils are above 70% waste recycling",
+                    },
+                    {
+                        "src_facebook": "scoring/img/social-graphics/england/facebook-7@2x.png",
+                        "src_instagram": "scoring/img/social-graphics/england/instagram-7@2x.png",
+                        "alt": "England Falling Behind; East Devon is the only English council that produces less than 300kg of residual waste per household",
+                    },
+                    {
+                        "src_facebook": "scoring/img/social-graphics/england/facebook-8@2x.png",
+                        "src_instagram": "scoring/img/social-graphics/england/instagram-8@2x.png",
+                        "alt": "England Average Score by Council Type; 35% for Single Tier, 29% for District, 35% for County, 45% for Mayoral Authority",
+                    },
+                ],
+            },
+            "wales": {
+                "pdf": {
+                    "src_pdf": "scoring/img/social-graphics/wales/scorecards-wales.pdf",
+                    "src_jpg": "scoring/img/social-graphics/wales/wales-graphic.jpg",
+                    "height": 1159,
+                    "width": 2100,
+                },
+                "zip": "scoring/img/social-graphics/wales/wales-graphics.zip",
+                "images": [
+                    {
+                        "src_facebook": f"scoring/img/social-graphics/wales/facebook-{i}@2x.png",
+                        "src_instagram": f"scoring/img/social-graphics/wales/instagram-{i}@2x.png",
+                        "alt": f"Wales Overview Graphic {i}",
+                    }
+                    for i in range(1, 6)  # Assuming 5 graphics
+                ],
+            },
+        }
+
+        nation = nations.get(self.kwargs["nation_name"].lower())
+
+        if not nation:
+            raise Http404("Page not found")
+
+        context["nation"] = nation
+        context["social_graphics"] = social_graphics.get(nation["slug"])
+        context["plan_year"] = self.request.year
+        context["page_title"] = nation["name"]
+        context["current_page"] = "nation-detail"
+
+        return context
