8929412a713e77a2ef222cf9efd0b59a498d4ed7
Struan Donald <struan@exo.org.uk>
2025-06-10 13:51:11 +0100

    [scorecards] utility method to fetch year config, with defaults fallback

--- scoring/defaults.py
+++ scoring/defaults.py
@@ -1,3 +1,5 @@
+from scoring.models import PlanYearConfig
+
 SECTION_WEIGHTINGS = {
     "Buildings & Heating": {
         "single": 20,
@@ -135,3 +137,17 @@ ORGANISATIONS = [
     {"name": "Winchester Action on Climate Change"},
     {"name": "Zap Map"},
 ]
+
+VALID = ["ORGANISATIONS", "SECTION_WEIGHTINGS"]
+
+
+def get_config(key, year):
+    conf = None
+    try:
+        conf = PlanYearConfig.objects.get(name=key, year__year=year)
+    except PlanYearConfig.DoesNotExist:
+        u_key = key.upper()
+        if u_key in VALID:
+            conf = globals()[u_key]
+
+    return conf
