e69fdd3b765e8bccc94e3c204a3a8ba8dd11523c
Struan Donald <struan@exo.org.uk>
2025-07-15 15:01:44 +0100

    update import filters to use json data, add commit and year options

--- caps/management/commands/import_filter_data.py
+++ caps/management/commands/import_filter_data.py
@@ -1,79 +1,88 @@
 import os
-import pandas as pd
-
-import requests
-import urllib3
-
-import ssl
-
-ssl._create_default_https_context = ssl._create_unverified_context
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
-
 from os.path import join
 
-from scoring.models import PlanScore
+import pandas as pd
+from django.conf import settings
+
+from caps.import_utils import BaseImportCommand
 from caps.models import PlanDocument
+from scoring.models import PlanScore
 
-FILTER_DATA_URL = "https://raw.githubusercontent.com/mysociety/climate_league_filter_datasets/main/data/outputs/all_filters.csv"
-FILTER_CSV = join(settings.DATA_DIR, "all_filters.csv")
+FILTER_JSON = join(settings.DATA_DIR, "league_filters.json")
 
-YEAR = settings.PLAN_YEAR
-
-
-def get_data():
-    """
-    Download filter_data_url
-    """
-    pd.read_csv(FILTER_DATA_URL).to_csv(FILTER_CSV, index=False)
+YELLOW = "\033[33m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+BLUE = "\033[36;4m"
+NOBOLD = "\033[0m"
 
 
-def import_filters():
-    ruc_lookup = dict((desc, code) for code, desc in PlanScore.RUC_TYPES)
-
-    updates = []
-    df = pd.read_csv(FILTER_CSV)
-    df["council-quintile"] = df["council-quintile"].fillna(0).astype(int)
-    df["pop_bucket"] = df["pop_bucket"].fillna("")
-    df["political_control"] = df["political_control"].fillna("")
-
-    for index, row in df.iterrows():
-        try:
-            plan_score = PlanScore.objects.get(
-                year=YEAR, council__authority_code=row["local-authority-code"]
-            )
-        except PlanScore.DoesNotExist:
-            print(
-                "could not find plan score for council {}".format(
-                    row["local-authority-code"]
-                )
-            )
-            continue
-
-        try:
-            cluster = ruc_lookup[row["ruc_cluster"]]
-        except KeyError:
-            cluster = ""
-
-        plan_score.population = row["pop_bucket"]
-        plan_score.deprivation_quintile = row["council-quintile"]
-        plan_score.political_control = row["political_control"]
-        plan_score.ruc_cluster = cluster
-        updates.append(plan_score)
-
-    PlanScore.objects.bulk_update(
-        updates,
-        ["population", "deprivation_quintile", "political_control", "ruc_cluster"],
-    )
-
-
-class Command(BaseCommand):
+class Command(BaseImportCommand):
     help = "fetch council filter data"
 
-    def handle(self, *args, **options):
-        print("getting the filter csv")
-        get_data()
+    YEAR = settings.PLAN_YEAR
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--year",
+            action="store",
+            help=f"Override default year ({self.YEAR}) to import plans from",
+        )
+
+        parser.add_argument(
+            "--commit",
+            action="store_true",
+            help="Make changes to database",
+        )
+
+    def get_data(self):
+        return pd.read_json(FILTER_JSON, orient="records")
+
+    def import_filters(self):
+        ruc_lookup = dict((desc, code) for code, desc in PlanScore.RUC_TYPES)
+
+        updates = []
+        df = self.get_data()
+        df["council-quintile"] = df["council-quintile"].fillna(0).astype(int)
+        df["pop_bucket"] = df["pop_bucket"].fillna("")
+
+        for index, row in df.iterrows():
+            try:
+                plan_score = PlanScore.objects.get(
+                    year=self.YEAR, council__authority_code=row["local-authority-code"]
+                )
+            except PlanScore.DoesNotExist:
+                self.stdout.write(
+                    f"{RED}could not find plan score for council {row['local-authority-code']}{NOBOLD}"
+                )
+                continue
+
+            try:
+                cluster = ruc_lookup[row["ruc_cluster"]]
+            except KeyError:
+                cluster = ""
+
+            plan_score.population = row["pop_bucket"]
+            plan_score.deprivation_quintile = row["council-quintile"]
+            plan_score.ruc_cluster = cluster
+            updates.append(plan_score)
+
+        PlanScore.objects.bulk_update(
+            updates,
+            ["population", "deprivation_quintile", "ruc_cluster"],
+        )
+
+    def handle(self, *args, year: int = None, commit: bool = False, **options):
         print("importing the filters")
-        import_filters()
+        if year:
+            self.YEAR = year
+
+        self.stdout.write(f"Updating filter values for {BLUE}{self.YEAR}{NOBOLD}")
+
+        if not commit:
+            self.stdout.write(
+                f"{YELLOW}Not updating database, call with --commit to do so{NOBOLD}"
+            )
+
+        with self.get_atomic_context(commit):
+            self.import_filters()
